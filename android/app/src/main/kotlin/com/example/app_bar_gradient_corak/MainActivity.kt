e9587a91d218046fca6f2b72be36cc5214d49107
amril syaifa yasin <amrilsyaifa@gmail.com>
2020-06-09 09:11:23 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/app_bar_gradient_corak/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_bar_gradient_corak
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+        leading: Icon(Icons.adb, color: Colors.white),
+        title: Text(
+          "Yasin Tech",
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.settings), onPressed: () {}),
+          IconButton(icon: Icon(Icons.home), onPressed: () {})
+        ],
+        flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color(0xff0096ff), Color(0xff6610f2)],
+                    begin: FractionalOffset.topLeft,
+                    end: FractionalOffset.bottomRight),
+                image: DecorationImage(
+                    image: AssetImage("assets/pattern.png"),
+                    fit: BoxFit.none,
+                    repeat: ImageRepeat.repeat))),
+      )),
+    );
+  }
+}
